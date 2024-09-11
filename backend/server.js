f27f43d78f4ead1604b628b43fd4e1b397dbff40
Robert Gleason <l2ob122@gmail.com>
2024-09-10 22:35:54 -0700

    Add auth controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
