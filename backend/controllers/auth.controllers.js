8bb96a9aecec47ea73eb6f4bc85b59fc5e0a6877
Robert Gleason <l2ob122@gmail.com>
2024-09-11 18:51:52 -0700

    added logic to login and logout

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,12 +2,37 @@ import User from '../models/user.model.js';
 import bcrtpt from 'bcryptjs';
 import generateToken from '../utils/generateToken.js';
 
-export const login = (req, res) => {
-  console.log('Login user');
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrtpt.compare(password, user?.password || '');
+
+    if (!user || !validPassword) {
+      return res.status(400).json({ message: 'Invalid username or password' });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log('Error in login controller', error.message);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log('Logout user');
+  try {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 };
 
 export const signup = async (req, res) => {
