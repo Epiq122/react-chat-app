fe5d9e0f0f2e58c264d768061ebfbeae5d15810d
Robert Gleason <l2ob122@gmail.com>
2024-09-11 20:49:07 -0700

    created signup compnent and styled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Signup />
     </div>
   );
 }
