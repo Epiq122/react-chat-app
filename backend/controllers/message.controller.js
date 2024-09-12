0eaf891e26a1d3030a10d02feaccf771a3fd08a9
Robert Gleason <l2ob122@gmail.com>
2024-09-11 19:50:50 -0700

    created the getmessages endpoint and added logic to get messages from users

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -37,3 +37,25 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: 'Something went wrong' });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate('messages'); // not a reference, but the actual message
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log('Error in getMessages controller', error.message);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
